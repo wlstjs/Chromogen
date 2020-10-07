250a8128a59be8cf4b9ad0c05ea82373bf6f68cb
Jinseon Shin <jinseonshin@gmail.com>
2020-10-07 14:34:30 -0400

    updatd hooks-output

--- package/hooks_generator/hooks_src/output/hooks-output.ts
+++ package/hooks_generator/hooks_src/output/hooks-output.ts
@@ -1,18 +1,16 @@
 /* eslint-disable */
 import type { Ledger } from '../hook-types.ts';
 import {
-    ledgerCurrState,
-    ledgerSetStateCallback,
-    ledgerInitialState,
-    ledgerPrevState,
-    importHooksInitialState,
-    importHooksCallback,
-    testHooksSetState,
-  } from './hooks-output-utils';
-
+  ledgerCurrState,
+  ledgerSetStateCallback,
+  ledgerInitialState,
+  ledgerPrevState,
+  importHooksInitialState,
+  importHooksCallback,
+  testHooksSetState,
+} from './hooks-output-utils';
 
 /* eslint-enable */
-
 
 /* ----- HELPERS ----- */
 
@@ -22,14 +20,13 @@ import {
 //readableHook = state of useState
 
 export const output = ({
-    initialState,
-    prevState,
-    currState,
-    setStateCallback,
-    count,
-}) => 
-)
-import { renderHook } from '@testing-library/react-hooks'
+  initialState,
+  prevState,
+  currState,
+  setStateCallback,
+  count,
+}: Ledger<string, any, SerializableParam>): string =>
+  `import { renderHook } from '@testing-library/react-hooks'
 // import React, { useState } from 'react';
 // import { ${importHooksInitialState}}
 
@@ -37,4 +34,4 @@ describe('Initial Render', () => {});
 
 describe('USESTATE', () => {});
 
-describe('USESTATE CALLBACKS', () => {});
+describe('USESTATE CALLBACKS', () => {});`;
